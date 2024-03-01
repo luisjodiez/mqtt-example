b5d5531b8b4bb36981006941e1bc336ceda0bc75
Luisjo Diez <luisjodiez@outlook.com>
2024-03-01 18:56:06 +0100

    added backend to persist state for the CI

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,13 @@
+
+# Set up backend for aws provider using s3 bucket and dynamodb table
+terraform {
+  backend "s3" {
+    bucket = "ljdb-tfstate-bucket"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
 terraform {
   required_providers {
     aws = {
