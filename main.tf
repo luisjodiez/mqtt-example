f03f64e1e90d57dca5912aea1d2fe8b89a229e94
Luisjo Diez <luisjodiez@outlook.com>
2024-03-01 20:25:16 +0100

    IAM policy fix for cloudwatch logging

--- main.tf
+++ main.tf
@@ -36,3 +36,8 @@ resource "aws_iam_role" "lambda_role" {
         ]
     })
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
