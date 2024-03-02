6776e65dec5dacd25e52f5f90ed3de578bbe1c44
Luisjo Diez <luisjodiez@outlook.com>
2024-03-02 19:42:38 +0100

    Updated server files

--- server.tf
+++ server.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "server_lambda" {
     handler       = "server.lambda_handler"
     role          = aws_iam_role.lambda_role.arn
     filename      = var.server_filename
-    source_code_hash = "N3k3Jn0eCg7X1gPYziG8QZ/LTkvorqy/OsnkRIxsMuY="
+    source_code_hash = "wu9f0Maq/uuEcAncmdTnrb055Jw660zJDdLLGn+FzXU="
     environment {
         variables = {
             USERNAME       = var.mqtt_username
